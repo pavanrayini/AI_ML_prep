b17855b1ce0d3d4e9970ffc125fc5d39b2792f96
pavanrayini <56913199+pavanrayini@users.noreply.github.com>
2026-09-01 22:35:43 +0530

    Update chatbot_non_ml.py

--- 02_chatbot/chatbot_non_ml.py
+++ 02_chatbot/chatbot_non_ml.py
@@ -1,21 +1,25 @@
-'''Basic Non ML chat bot! with multiple items (dictionary)'''
-print("Welcome to Basic Non ML chat bot application with multiple items!")
+'''Basic Non ML chat bot! with multiple items (dictionary'''
+print("Welcome to Basic Non ML chat bot application with multiple items! \n"
+     "improvements: \n"
+     "1) storing the username and not overwriting it \n"
+     "2) case insentivity for product entered by user \n"
+     "3) exiting with bye for how_many rather than user \n"
+     "4) improved menu interface")
+print("in next update: multiple items in cart and bill generation")
 products = {
     "apple" : 25,
     "orange" : 15}
+your_name = None
 while(True):
     user = input("You : ")
-    your_name = None
     if "hello" in user.lower() or "hi" in user.lower() or "hey" in user.lower() or "how are you" in user.lower():
         print("Bot : Hi Sir/Madam, could you help enter your name")
         your_name = input("your name : ")
         print('Bot : Hi ', your_name,'!, how can i help you today! :)'
               ' please enter product you want to buy from these ')
-        i = 1;
-        for x in products:
-            print(i,") ", x)
-            i=i+1
-        product_entered = input("enter the product : ")
+        for product, price in products.items():
+            print(f"{product} - ${price}")
+        product_entered = input("enter the product : ").lower()
         if product_entered in products:
             print("Bot: cost of ", product_entered,"is ", products[product_entered]," rupees per piece, how many do you want")
             how_many = input("how_many : ")
@@ -24,7 +28,7 @@ while(True):
                     print("Bot: cost of ", how_many," ", product_entered,"s would be", products[product_entered] * int(how_many),
                           " rupees in total")
                     break
-                elif "bye" in user.lower() or "end" in user.lower() or "." in user.lower():
+                elif "bye" in how_many.lower() or "end" in how_many.lower() or "." in how_many.lower():
                     if your_name:
                         print("Bye ", your_name)
                         break
